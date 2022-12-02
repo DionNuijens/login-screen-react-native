432844fc854c695fc41ce0c2946f0dec4cfb5ae2
mino <amine222@hotmail.nl>
2022-12-02 22:06:23 +0100

    12:40

--- login-screen-react-native/components/Header.js
+++ login-screen-react-native/components/Header.js
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useLinkProps } from '@react-navigation/native'
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <Text style={styles.textname}>
+        {props.name}
+      </Text>
     </View>
   )
 }
 
 export default Header
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 15,
+    },
+    textname: {
+        fontWeight:'bold',
+        fontSize: 28,
+        
+    },
+})
