b63f5a1701a75c9e05198e83c98f0f1dc81f6211
mino <amine222@hotmail.nl>
2022-12-02 22:06:14 +0100

    12:40

--- login-screen-react-native/App.js
+++ login-screen-react-native/App.js
@@ -1,25 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState, useEffect} from "react";
+import { firebase } from "./firebase/firebase-config.js"
 
-import { authentication } from './firebase/firebase-config';
+import LoginScreen from "./src/LoginScreen.js";
+import SignUpScreen from "./src/SignUpScreen.js";
+import HomeScreen from "./src/HomeScreen.js";
+import Header from "./components/Header.js";
+import { NativeAppEventEmitter } from "react-native";
 
-// app
+const Stack = createNativeStackNavigator();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>deez nuts</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App(){
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if(initializing) setInitializing(false);
+  }
+  useEffect(() =>  {
+    const user = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return user;
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name = 'Login' 
+          component={LoginScreen}
+          options = {{
+            headerTitle: () => <Header name = "Klarity"/>,
+            headerStyle: {
+              height: 150,
+              borderBottomLeftRadius:50,
+              borderBottomRightRadius:50,
+              backgroundColor
+
+              // 12:40
+            }
+          }}
+        
+      />
+      </Stack.Navigator>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
