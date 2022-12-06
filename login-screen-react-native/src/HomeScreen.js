d52bef596abd4ee2d8edf506d0eeb0e88232d7cd
amine <amine.dbh99@gmail.com>
2022-12-06 13:28:51 +0100

    aa

--- login-screen-react-native/src/HomeScreen.js
+++ login-screen-react-native/src/HomeScreen.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, useWindowDimensions  } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Image, useWindowDimensions  } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import spongebob from '../assets/spongebob.jpg'
+import { firebase } from '../config'
 
 
 const HomeScreen = () => {
+
+  const [userName, setUserName] = useState('')
   const {height} = useWindowDimensions()
+
+  useEffect(() => {
+    
+  })
 
   return (
     <SafeAreaView>
