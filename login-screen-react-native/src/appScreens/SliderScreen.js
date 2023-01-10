fb4c00ff08f3a87a46d3138b282ad4deaed82702
amine <amine.dbh99@gmail.com>
2023-01-10 13:42:15 +0100

    a

--- login-screen-react-native/src/appScreens/SliderScreen.js
+++ login-screen-react-native/src/appScreens/SliderScreen.js
@@ -1,34 +1,11 @@
 import React, { useState, useEffect} from "react";
-import {AppRegistry, StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Image, useWindowDimensions, onPress, TextInput, Animated, Dimensions} from 'react-native';
-import { useNavigation, getStateFromPath } from '@react-navigation/native';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
 import Slider from '@react-native-community/slider'
-// import LinearGradient from 'react-native-linear-gradient';
-// import {Slider} from '@miblanchard/react-native-slider'; 
 import { LinearGradient } from 'expo-linear-gradient';
 
-// import ColorPicker, {
-//     Preview,
-//     OpacitySlider,
-//     BrightnessSlider,
-//     HueSlider,
-//     SaturationSlider,
-//   } from 'reanimated-color-picker';
-
   const SliderScreen = () => {
-    
-    // const [value, setValue] = useState(0);
-    const [value, setValue] = useState(0);
 
-    // const [hue, setHue] = useState(0);
-    // const backgroundColor = new Animated.Value(0);
-  
-    // useEffect(() => {
-    //   backgroundColor.setValue(hue);
-    // }, [hue]);
-  
-    // const [minValue, setMinValue] = useState(0);
-    // const [maxValue, setMaxValue] = useState(100);
-  
+    const [value, setValue] = useState(0);
 
     return(
         <View style={styles.container}>
@@ -36,57 +13,11 @@ import { LinearGradient } from 'expo-linear-gradient';
         <Text style={{height: 30}}>
          {value}
         </Text>
-        {/* <Slider
-        minimumValue={0}
-        maximumValue={2}
-        step={1}
-        value={value}
-        onValueChange={setValue}
-        style={{ width: 300 }}
-      /> */}
-
-      {/* <LinearGradient colors={['#ef32d9', '#89ffd']} style={{with:'100%',
-      justifyContent: 'center'}}>
-      
-      <LinearGradient
-      colors={['red', 'yellow', 'lightblue', 'lightgreen']}
-      style={styles.gradient}
-      start={{ x: 0, y: 0}}
-      end={{  x: 1, y: 0 }}
-      >
-      <Text>laskdjflaksjl</Text>
-
-      </LinearGradient>
-      </LinearGradient> */}
-
-      {/* <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <View
-          style={{
-            width: 100,
-            height: 100,
-            backgroundColor: value === 0 ? 'red' : 'grey',
-          }}
-        />
-        <View
-          style={{
-            width: 100,
-            height: 100,
-            backgroundColor: value === 1 ? 'yellow' : 'grey',
-          }}
-        />
-        <View
-          style={{
-            width: 100,
-            height: 100,
-            backgroundColor: value === 2 ? 'green' : 'grey',
-          }}
-        />
-      </View> */}
 
       <LinearGradient colors={['#00ff00','#FBFF00','#FFA200','#ff0000']}
       start={{ x: 0, y: 0}}
       end={{  x: 1, y: 0 }}
-      style= {{    borderRadius: 20,}}
+      style= {{borderRadius: 20,}}
       >
   <Slider
     style={{ width: Dimensions.get('window').width - 50, maxHeight: '2%' }}
@@ -102,27 +33,6 @@ import { LinearGradient } from 'expo-linear-gradient';
     
   />
 </LinearGradient>
-
-      {/* <View style={styles.container}>
-      <LinearGradient
-        colors={['#ff0000', '#00ff00', '#0000ff']}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }}
-        style={styles.gradient}
-      >
-        <Slider
-          minimumValue={0}
-          maximumValue={1}
-          value={value}
-          onValueChange={setValue}
-          style={styles.slider}
-          thumbTintColor="#fff"
-          minimumTrackTintColor="#transparent"
-          maximumTrackTintColor="#transparent"
-        />
-      </LinearGradient>
-      <Text>Value: {value}</Text>
-    </View> */}
     </View>
     )
 }
@@ -149,37 +59,3 @@ const styles = StyleSheet.create({
     ],
   },
 });
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 2,
-//         marginTop: '115%',
-//         borderBottomLeftRadius: 0,
-//         borderBottomRightRadius: 0,
-//         borderTopRightRadius: 20,
-//         borderTopLeftRadius: 20,
-//         backgroundColor: '#f4f4f4',
-//         alignItems: 'center',
-//     },
-//     shadow: {
-//         shadowColor: '#000',
-//         shadowOffset: {
-//           width: 0,
-//           height: 2,
-//         },
-//     },
-//     linearGradient: {
-//       borderRadius: 20,
-//       height: 15,
-//       justifyContent: 'center',
-//     },
-//      brightThumb: {
-//       backgroundColor: COLORS.WHITE,
-//       borderColor: COLORS.GREY,
-//       borderRadius: 10,
-//       borderWidth: 5,
-//       height: 20,
-//       width: 20,
-//      },
-
-// });
